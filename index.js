a4c3205cba7f66d902ce9a3e97e92d5b368c2115
ShafinRME <shafin.rmedu4@gmail.com>
2022-05-16 00:23:18 +0600

    modal data is sending in DB

--- index.js
+++ index.js
@@ -19,11 +19,29 @@ async function run() {
     try {
         await client.connect();
         const serviceCollection = client.db('doctors_portal').collection('services');
+        const bookingCollection = client.db('doctors_portal').collection('bookings');
+
+
         app.get('/service', async (req, res) => {
             const query = {};
             const cursor = serviceCollection.find(query);
             const services = await cursor.toArray();
             res.send(services)
+        });
+        /**
+        * API naming conventions
+        * app.get('/booking') //get all booking in this collection or getting more than 1 or by filter
+        * app.get('/booking/:id') // get a specific booking
+        * app.post('/booking') // add a new booking
+        * app.patch('/booking/:id) //update a specific booking
+        * app.delete('/booking/:id) // specific delete booking
+        */
+
+        app.post('/booking', async (req, res) => {
+            const booking = req.body;
+            const query = { treatment: booking.treatment, date: booking.date, patient: booking.patient }
+            const result = await bookingCollection.insertOne(booking);
+            res.send(result);
         })
 
     }
