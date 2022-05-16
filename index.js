323967aad3c38faa76c66b5616046e3cb6d60a07
ShafinRME <shafin.rmedu4@gmail.com>
2022-05-16 21:41:54 +0600

    Put method is running

--- index.js
+++ index.js
@@ -20,6 +20,7 @@ async function run() {
         await client.connect();
         const serviceCollection = client.db('doctors_portal').collection('services');
         const bookingCollection = client.db('doctors_portal').collection('bookings');
+        const userCollection = client.db('doctors_portal').collection('users');
 
 
         app.get('/service', async (req, res) => {
@@ -29,6 +30,17 @@ async function run() {
             res.send(services)
         });
 
+        app.put('/user/:email', async (req, res) => {
+            const email = req.params.email;
+            const user = req.body;
+            const filter = { email: email };
+            const options = { upsert: true };
+            const updateDoc = {
+                $set: user,
+            };
+            const result = await userCollection.updateOne(filter, updateDoc, options);
+            res.send(result);
+        })
         app.get('/available', async (req, res) => {
             const date = req.query.date;
             const services = await serviceCollection.find().toArray();
@@ -49,6 +61,7 @@ async function run() {
         * app.get('/booking/:id') // get a specific booking
         * app.post('/booking') // add a new booking
         * app.patch('/booking/:id) //update a specific booking
+        * app.put('/booking/:id) // upert ==> update (if exists) or insert (if doesn't exists)
         * app.delete('/booking/:id) // specific delete booking
         */
 
